18a05786f101e26253ab1590b073b04b23d2f1f1
SrinuKatama <59701542+SrinuKatama@users.noreply.github.com>
2020-06-30 18:34:43 +0530

    [Srinu]Created repository classes

---
+++ Patient/src/main/java/com/example/demo/Utility/JWTutil.java
@@ -0,0 +1,43 @@
+package com.example.demo.Utility;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Service
+public class JWTutil
+{
+private static final String SECRET = "9886002418";
+	
+     /* Method to generate the token for the particular userId */
+	public String jwtToken(long l) {
+		String token = null;
+		try {
+			token = JWT.create().withClaim("id", l).sign(Algorithm.HMAC512(SECRET));
+		} catch (IllegalArgumentException | JWTCreationException e) {
+
+			e.printStackTrace(); 
+		}
+		return token;
+	}
+
+	/*
+	 * method to parse the jwt token into integer
+	 * */
+	public int parseJWT(String jwt) {
+
+		Integer userId = 0;
+		if (jwt != null) {
+			try {
+				userId = JWT.require(Algorithm.HMAC512(SECRET)).build().verify(jwt).getClaim("id").asInt();
+			} catch (JWTVerificationException | IllegalArgumentException e) {
+				e.printStackTrace();
+			}
+		}
+		return userId;
+	}
+
+}
